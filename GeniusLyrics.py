002aa2decd9062166e0dda3651721a60d426724f
compmix <ngfelixc@gmail.com>
2016-07-31 14:34:26 -0700

    Fixed up scraping
    Cleared out an ad link in the middle

--- GeniusLyrics.py
+++ GeniusLyrics.py
@@ -16,7 +16,13 @@ class GeniusLyrics:
 		song_url = search_json['response']['hits'][0]['result']['url']
 		response = requests.get(song_url, headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.153 Safari/537.36'})
 		soup = BeautifulSoup(response.text, "lxml")
-		lyrics = soup.find("div", class_="song_body-lyrics").get_text()
+		soup = soup.find("div", class_="song_body-lyrics")
+		soup.find("div", class_="dfp_unit").decompose()
+		soup.find("share-buttons").decompose()
+		soup.find(class_="u-xx_large_top_margin u-bottom_margin").decompose()
+
+		#print(soup.prettify())
+		lyrics = soup.get_text()
 		return lyrics
 
 
@@ -25,9 +31,10 @@ if __name__ == '__main__':
 	tok = f.readline()
 	genius = GeniusLyrics(tok)
 
-	print(tok)
-
 	while True:
 		query = input('What song to search lyrics for? ')
 		lyrics = genius.search(query)
 		print(lyrics)
+
+
+#TODO: fix proper authentication
